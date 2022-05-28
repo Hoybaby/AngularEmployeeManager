a4411c2ad8951672a73a806c45589713a63e21c2
Hoybaby <mbartek436@gmail.com>
2022-05-27 22:34:16 -0400

    trying to display some of my sample code i have in the backend to be shown to the front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -7,17 +8,27 @@ import { EmployeeService } from './employee.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 
   public employees: Employee[] = [];;
 
   constructor(private employeeService: EmployeeService) { };
 
+  //whwnever intialized this will start.
+  ngOnInit() {
+    this.getEmployees();
+  }
+
   public getEmployees(): void {
+    // this will make a request over the network/internet it is on the obersable
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
+        // this will be called if the request is successful
         this.employees = response;
+      },
+      (error: HttpErrorResponse) => { 
+        alert(error.message);
       }
     );
   }
