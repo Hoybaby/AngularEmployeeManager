958a20bc175d300f27284ff709980d0ed45f7781
Hoybaby <mbartek436@gmail.com>
2022-05-27 22:16:44 -0400

    inital coomit to github. I already started to work on my code

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+
+//just defining the interface
+export interface Employee {
+    id: number;
+    name: string;
+    emaik: string;
+    jobTitle: string;
+    imageUrl: string;
+    emmployeeCode: string;
+}
